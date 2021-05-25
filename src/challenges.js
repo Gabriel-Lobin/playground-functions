d2c4fdcd0d267a66bb802e915dd116ea8ccfc3a3
Gabriel S Lobin <gabrielsuassuna@hotmail.com>
2021-05-25 18:46:19 -0300

    Questoes 7 e 8 resolvidas

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,10 @@
 // Desafio 1
-function compareTrue(num1, num2) {
-  if (num1 === true && num2 === true ){
-    return true
-  } else {
-    return false
-  }
+function compareTrue(num1, num2){
+  let verdade = false;
+	if(num1 === true && num2 === true ){
+		verdade = true;
+	}
+  return verdade 
 }
 // Desafio 2
 function calcArea(base, height) {
@@ -66,15 +66,39 @@ let repeticoes = 0;
 console.log(highestCount(arrayteste));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+function catAndMouse(mouse, cat1, cat2) {
+ 	let distancia1 = cat1 - mouse;
+ 	let distancia2 = cat2 - mouse;
+	let cat2Mouse = Math.abs(distancia2);
+	let cat1mouse = Math.abs(distancia1);
 
+ if (cat2Mouse === cat1mouse){
+	return 'os gatos trombam e o rato foge';
+ } else if (distancia1 > distancia2){
+	 return 'cat2';
+	}	else if (distancia1 < distancia2){
+	return 'cat1';
+ }	
+}
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+	let tudo = [];		
+		for(let index = 0; index < array.length; index += 1){
+			
+			if(array[index] % 3 === 0 && array[index] % 5 === 0){
+				tudo.push('fizzBuzz');
+			} else if(array[index] % 5 === 0) {
+				tudo.push('buzz');
+			} else if(array[index] % 3 === 0){
+				tudo.push('fizz');
+			} else {
+				tudo.push('bug!');
+			}
+		}
+	return tudo;
 }
 
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 9
 function encode() {
   // seu código aqui
